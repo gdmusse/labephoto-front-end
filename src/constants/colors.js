dbcb63a28824c311798acde86f0b6ba6d0e37335
Gabriel Musse <gdmusse@hotmail.com>
2021-06-30 15:26:16 -0300

    sign up page

---
+++ src/constants/colors.js
@@ -0,0 +1,2 @@
+export const primaryColor = "#4287f5";
+export const neutralColor = "#474749";
---
+++ src/pages/SignUpPage/SignUpForm.js
@@ -0,0 +1,117 @@
+import React, { useContext } from "react";
+import { InputsContainer, SignUpFormContainer } from "./styled";
+import TextField from "@material-ui/core/TextField";
+import useForm from "../../hooks/useForm";
+import Button from "@material-ui/core/Button";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import BASE_URL from "../../constants/urls";
+import { goToFeed } from "../../routes/coordinator";
+import GlobalStateContext from "../../global/GlobalStateContext";
+import Loader from "../../components/Loader";
+import { withStyles } from "@material-ui/core";
+
+const SignUpForm = () => {
+  const history = useHistory();
+  const [form, onChange, clear] = useForm({
+    name: "",
+    nickname: "",
+    email: "",
+    password: "",
+  });
+
+  const { setOpenAlert, setAlertMsg, setAlertSeverity, loading, setLoading } =
+    useContext(GlobalStateContext);
+
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+    signUp(form, clear, history);
+    setLoading(true);
+  };
+
+  const signUp = (body, clear, history) => {
+    setLoading(true);
+    axios
+      .post(`${BASE_URL}/user/signup`, body)
+      .then((res) => {
+        localStorage.setItem("token", res.data.token);
+        clear();
+        setLoading(false);
+        /*    goToFeed(history); */
+      })
+      .catch((err) => {
+        setLoading(false);
+        setAlertMsg(err.response.data.error);
+        setAlertSeverity("error");
+        setOpenAlert(true);
+      });
+  };
+
+  return (
+    <SignUpFormContainer>
+      {loading ? (
+        <Loader />
+      ) : (
+        <form onSubmit={onSubmitForm}>
+          <InputsContainer>
+            <TextField
+              name={"name"}
+              value={form.name}
+              onChange={onChange}
+              label={"Name"}
+              variant={"outlined"}
+              fullWidth
+              margin={"normal"}
+              required
+              autoFocus
+            />
+            <TextField
+              name={"nickname"}
+              value={form.nickname}
+              onChange={onChange}
+              label={"Nickname"}
+              variant={"outlined"}
+              fullWidth
+              margin={"normal"}
+              required
+              autoFocus
+            />
+            <TextField
+              name={"email"}
+              value={form.email}
+              onChange={onChange}
+              label={"E-mail"}
+              variant={"outlined"}
+              fullWidth
+              margin={"normal"}
+              required
+              type={"email"}
+            />
+            <TextField
+              name={"password"}
+              value={form.password}
+              onChange={onChange}
+              label={"Password"}
+              variant={"outlined"}
+              fullWidth
+              margin={"normal"}
+              required
+              type={"password"}
+              InputProps={{ inputProps: { minLength: 6 } }}
+            />
+          </InputsContainer>
+          <Button
+            type={"submit"}
+            fullWidth
+            variant={"contained"}
+            color={"primary"}
+          >
+            Sign Up
+          </Button>
+        </form>
+      )}
+    </SignUpFormContainer>
+  );
+};
+
+export default SignUpForm;
